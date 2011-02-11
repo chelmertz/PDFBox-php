9b57befe6bac0beeab7922f5a44a1eea9e828446
Carl Helmertz <helmertz@gmail.com>
2011-02-12 00:20:41 +0100

    Add: test files, including the currently latest jar

---
+++ demos/demo.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '..'.DIRECTORY_SEPARATOR.'PDFBox.php';
+require_once '..'.DIRECTORY_SEPARATOR.'PDFBox'.DIRECTORY_SEPARATOR.'ExtractText.php';
+
+$jar = "pdfbox-app-1.4.0.jar";
+$pdf_box = new PDFBox($jar);
+$extract_text = new PDFBox\ExtractText($pdf_box);
+$extract_text->setOutputFilename('my_regular_pdf.txt');
+
+$extract_text->parse('regular.pdf');
